4c8c0f08fe517a5d892300212df384927dbbc036
孙振光 <light22@126.com>
2022-08-06 22:09:20 +0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setuptools.setup(
     version="1.0.2",    #包版本号，便于维护版本
     author="北极星光",    #作者，可以写自己的姓名
     author_email="light22@126.com",    #作者联系方式，可写自己的邮箱地址
+    license='MIT',
     description="方便快捷Excel操作",#包的简述
     long_description=long_description,    #包的详细介绍，一般在README.md文件内
     long_description_content_type="text/markdown",
     url="https://github.com/18513233125/Excel-operate",    #自己项目地址，比如github的项目地址
+    download_url = 'https://pypi.org/project/excel-operate-light22',
     packages=setuptools.find_packages(),
+    install_requires=['openpyxl'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
