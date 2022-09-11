c4ccc7954ce830fdeb8745405515ee31cc3ba9e1
孙振光 <light22@126.com>
2022-09-11 20:46:01 +0800

    modified:   excel_operate/sheet_comparison.py 	modified:   excel_operate/sheet_copy.py 	modified:   "tests/\346\257\224\350\276\203\347\244\272\344\276\213 - \345\257\271\346\257\224.xlsx"

--- excel_operate/sheet_comparison.py
+++ excel_operate/sheet_comparison.py
@@ -13,12 +13,11 @@ from openpyxl.styles import PatternFill, Alignment, Side, Border, Font
 
 
 class SheetComparison:
-    def __init__(self, src_excel, cmp_excel, report_path: str = None) -> None:
+    def __init__(self, src_excel: ExcelOperate, cmp_excel: ExcelOperate) -> None:
         '''===================================\n
         传入两个Excel对象，进行比较。生成报告
         src_excel: 原Excel对象，可传入ExcelOperate类对象
         cmp_excel: 待比较的Excel对象，可传入ExcelOperate类对象
-        report_path: 报告结果保存路径
         '''
         self.src_excel = src_excel
         self.cmp_excel = cmp_excel
@@ -51,13 +50,13 @@ class SheetComparison:
         if style == 'zeng':
             cell.font = Font(color='0000FF')
         if style == 'shan':
-            cell.font = Font(color='FF0000',strike=True)
+            cell.font = Font(color='FF0000', strike=True)
         if style == 'gai':
             cell.font = Font(color='FF00FF')
 
     def compare(self):
         '''===================================\n
-        对比工作表：将原工作表及目标工作表的表头行和关键列设置好之后即可使用此方法对比工作表，并生成对比报告。
+        对比工作表：将原工作表及目标工作表的表头行和关键列设置好之后即可使用此方法对比工作表，并返回对比报告。
         '''
         # 如果目标工作表的表头行与原工作表的表头行不在同一行
         if self.src_title_row != self.cmp_title_row:
@@ -86,7 +85,6 @@ class SheetComparison:
             i.value for i in self.src_excel.ws[self.src_title_row]]
         cmp_title_list = [
             i.value for i in self.cmp_excel.ws[self.cmp_title_row]]
-        # print(cmp_title_list)
         if src_title_list != cmp_title_list:
             self.src_excel.insert_rows(1)
             self.src_title_row += 1
@@ -124,6 +122,26 @@ class SheetComparison:
                 if j == None:
                     self.src_excel.ws[1][idx].value = '删'
                     self.cmp_excel.ws[1][idx].value = '临'
+
+            # 设置对齐、边框及字体颜色
+            for cell in self.src_excel.ws[1]:
+                cell.alignment = Alignment(
+                    horizontal='center', vertical='center')
+                cell.border = Border(left=Side('thin'), right=Side(
+                    'thin'), top=Side('thin'), bottom=Side('thin'))
+            for cell in self.cmp_excel.ws[1]:
+                cell.alignment = Alignment(
+                    horizontal='center', vertical='center')
+                cell.border = Border(left=Side('thin'), right=Side(
+                    'thin'), top=Side('thin'), bottom=Side('thin'))
+            for col in self.src_excel.ws.iter_cols():
+                if col[0].value == '删':
+                    for cell in col:
+                        self.font_color(cell, 'shan')
+            for col in self.cmp_excel.ws.iter_cols():
+                if col[0].value == '增':
+                    for cell in col:
+                        self.font_color(cell, 'zeng')
 
         # 如果目标工作表的关键列与原工作表的关键列不相同
         src_key_list = [i.value for idx, i in enumerate(
@@ -173,10 +191,78 @@ class SheetComparison:
                         # 将对比工作表前一顶的上方插入空白行，从而形成错行的效果
                         self.cmp_excel.insert_rows(
                             self.cmp_title_row + idx + 1 - n)
+        # 对比行，在原工作表行首写入标记同时设置字体颜色
+        self.src_excel.insert_cols(1, width=3)  # 在原工作表首列处插入地行空列用于标记'增','删','改'
+        for cell in self.src_excel.ws['A']:
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+            cell.border = Border(left=Side('thin'), right=Side(
+                'thin'), top=Side('thin'), bottom=Side('thin'))
+        self.src_key_col += 1
+        src_key_list = [i.value for idx, i in enumerate(
+            self.src_excel.ws[get_column_letter(self.src_key_col)], 1) if idx > self.src_title_row]
+        cmp_key_list = [i.value for idx, i in enumerate(
+            self.cmp_excel.ws[get_column_letter(self.cmp_key_col)], 1) if idx > self.cmp_title_row]
 
-            print(src_key_list)
-            print(cmp_key_list)
-            self.font_color(src_excel.ws['B3'], 'shan')
+        for idx, i, j in zip(range(self.src_title_row + 1, len(src_key_list) + self.src_title_row + 1), src_key_list, cmp_key_list):
+            if i == None != j:
+                self.src_excel.ws[f'A{idx}'].value = '增'
+            if i != None == j:
+                self.src_excel.ws[f'A{idx}'].value = '删'
+        # 逐项对比数据，标注改动的地方
+        for x in range(1, self.src_excel.ws.max_column):
+            for y in range(2, self.src_excel.ws.max_row + 1):
+                if self.src_excel.ws[f'{get_column_letter(x + 1)}{1}'].value not in ['增', '删', '临'] and self.src_excel.ws[f'A{y}'].value not in ['增', '删', '临'] and self.cmp_excel.ws[f'{get_column_letter(x)}{1}'].value not in ['增', '删', '临']:
+                    if self.src_excel.ws[f'{get_column_letter(x + 1)}{y}'].value != self.cmp_excel.ws[f'{get_column_letter(x)}{y}'].value:
+                        self.src_excel.ws[f'A{y}'].value = '改'
+                        self.font_color(self.src_excel.ws[f'A{y}'], 'gai')
+                        if self.src_excel.ws[f'{get_column_letter(x + 1)}{y}'].value == None:
+                            self.font_color(
+                                self.cmp_excel.ws[f'{get_column_letter(x)}{y}'], 'zeng')
+                        elif self.cmp_excel.ws[f'{get_column_letter(x)}{y}'].value == None:
+                            self.font_color(
+                                self.src_excel.ws[f'{get_column_letter(x + 1)}{y}'], 'shan')
+                        else:
+                            self.font_color(
+                                self.src_excel.ws[f'{get_column_letter(x + 1)}{y}'], 'gai')
+                            self.font_color(
+                                self.cmp_excel.ws[f'{get_column_letter(x)}{y}'], 'gai')
+        # 删除临时行
+        for i in range(self.src_excel.ws.max_column, 0, -1):
+            if self.src_excel.ws[f'{get_column_letter(i)}1'].value == '临':
+                self.src_excel.delete_cols(i)
+        for i in range(self.cmp_excel.ws.max_column, 0, -1):
+            if self.cmp_excel.ws[f'{get_column_letter(i)}1'].value == '临':
+                self.cmp_excel.delete_cols(i)
+
+        # 将原工作表及目标工作表合并后写入报告文件
+        self.src_excel.insert_rows(1, height=20)
+        self.src_excel.ws['A1'].value = '原 工 作 表'
+        self.src_excel.ws['A1'].font = Font(name='微软雅黑', size=16, b=True)
+        self.src_excel.ws['A1'].alignment = Alignment(
+            horizontal='center', vertical='center')
+        self.src_excel.ws.merge_cells(
+            f'A1:{get_column_letter(self.src_excel.ws.max_column)}1')
+        self.cmp_excel.insert_rows(1, height=20)
+        self.cmp_excel.ws['A1'].value = '目 标 工 作 表'
+        self.cmp_excel.ws['A1'].font = Font(name='微软雅黑', size=16, b=True)
+        self.cmp_excel.ws['A1'].alignment = Alignment(
+            horizontal='center', vertical='center')
+        self.cmp_excel.ws.merge_cells(
+            f'A1:{get_column_letter(self.cmp_excel.ws.max_column)}1')
+
+        rpt = ExcelOperate(sheet_name='对比结果')
+        cp = SheetCopy()
+        rpt = cp.copy_sheet(src_file=self.src_excel, tag_file=rpt)
+        rpt = cp.copy_sheet(src_file=self.cmp_excel, tag_file=rpt,
+                            origin_col=self.src_excel.ws.max_column + 1)
+        for row in rpt.ws.iter_rows():
+            if row[0].value == '增':
+                for cell in row:
+                    self.font_color(cell, 'zeng')
+            if row[0].value == '删':
+                for cell in row:
+                    self.font_color(cell, 'shan')
+        return rpt
 
 
 # 调试
@@ -184,9 +270,9 @@ if __name__ == '__main__':
     src_excel = ExcelOperate('tests\比较示例 - 原.xlsx')
     cmp_excel = ExcelOperate('tests\比较示例 - 对比.xlsx')
     report_path = 'D:/Desktop/对比报告.xlsx'
-    cmper = SheetComparison(src_excel, cmp_excel, report_path)
+    cmper = SheetComparison(src_excel, cmp_excel)
     cmper.set_title_row(2, 2)
     cmper.set_key_col(2, 2)
-    cmper.compare()
-    src_excel.save('D:/Desktop/1111src.xlsx')
-    cmp_excel.save('D:/Desktop/1111cmp.xlsx')
+    report = cmper.compare()
+    report.save(report_path)
+    print('对比完成！对比报告已保存至：', report_path)
--- excel_operate/sheet_copy.py
+++ excel_operate/sheet_copy.py
@@ -11,7 +11,7 @@ from openpyxl.utils import get_column_letter
 
 
 class SheetCopy:
-    def __init__(self, src_file_path: str, tag_file_path: str = None, sheet_name: str = None, column_adjust: float = 0) -> None:
+    def __init__(self, src_file_path: str = None, tag_file_path: str = None, sheet_name: str = None, column_adjust: float = 0) -> None:
         '''===================================\n
         src_file_path: 此参数为源Excel文件路径
         tag_file_path: 此参数为目标Excel文件路径，不指定的情况下默认新建工作薄
@@ -40,49 +40,54 @@ class SheetCopy:
         self.column_adjust = column_adjust
 
     # 定义工作表复制模块
-    def copy_sheet(self, origin_row: int = 1, origin_col: int = 1):
+    def copy_sheet(self, src_file: ExcelOperate = None, tag_file: ExcelOperate = None, origin_row: int = 1, origin_col: int = 1):
         '''===================================\n
         复制工作表
+        src_file: 原文件的ExcelOperate对象
+        tag_file: 目标文件的ExcelOperate对象
         origin_row: 设定目标工作表写入的起始行，默认从第1行开始写入，传入大于或等于1的整数则会从设定的行开始写入。
         origin_col: 设定目标工作表写入的起始列，默认从第1列开始写入，传入大于或等于1的整数则会从设定的列开始写入。
         '''
-        for row in self.src_file.ws:
+        if src_file == None:
+            src_file = self.src_file
+        if tag_file == None:
+            tag_file = self.tag_file
+        for row in src_file.ws:
             # 遍历源xlsx文件制定sheet中的所有单元格
             for cell in row:  # 复制数据
                 cell.row += origin_row - 1
                 cell.column += origin_col - 1
-                self.tag_file.ws[cell.coordinate].value = cell.value
+                tag_file.ws[cell.coordinate].value = cell.value
                 if cell.has_style:  # 复制样式
-                    self.tag_file.ws[cell.coordinate].font = copy(cell.font)
-                    self.tag_file.ws[cell.coordinate].border = copy(
-                        cell.border)
-                    self.tag_file.ws[cell.coordinate].fill = copy(cell.fill)
-                    self.tag_file.ws[cell.coordinate].number_format = copy(
+                    tag_file.ws[cell.coordinate].font = copy(cell.font)
+                    tag_file.ws[cell.coordinate].border = copy(cell.border)
+                    tag_file.ws[cell.coordinate].fill = copy(cell.fill)
+                    tag_file.ws[cell.coordinate].number_format = copy(
                         cell.number_format)
-                    self.tag_file.ws[cell.coordinate].protection = copy(
+                    tag_file.ws[cell.coordinate].protection = copy(
                         cell.protection)
-                    self.tag_file.ws[cell.coordinate].alignment = copy(
+                    tag_file.ws[cell.coordinate].alignment = copy(
                         cell.alignment)
 
-        merged_cells = self.src_file.ws.merged_cell_ranges  # 已合并的单元格列表
+        merged_cells = src_file.ws.merged_cell_ranges  # 已合并的单元格列表
         if len(merged_cells) > 0:  # 检测源xlsx中合并的单元格
             for merged_cell in merged_cells:
-                self.tag_file.ws.merge_cells(start_row=merged_cell.min_row + origin_row - 1, end_row=merged_cell.max_row + origin_row - 1,
-                                             start_column=merged_cell.min_col + origin_col - 1, end_column=merged_cell.max_col + origin_col - 1)  # 合并单元格
+                tag_file.ws.merge_cells(start_row=merged_cell.min_row + origin_row - 1, end_row=merged_cell.max_row + origin_row - 1,
+                                        start_column=merged_cell.min_col + origin_col - 1, end_column=merged_cell.max_col + origin_col - 1)  # 合并单元格
         # 开始处理行高列宽
-        for i in range(1, self.src_file.ws.max_row + 1):
-            self.tag_file.ws.row_dimensions[i + origin_row -
-                                            1].height = self.src_file.ws.row_dimensions[i].height
-        for i in range(1, self.src_file.ws.max_column + 1):
-            self.tag_file.ws.column_dimensions[get_column_letter(
-                i + origin_col - 1)].width = self.src_file.ws.column_dimensions[get_column_letter(i)].width + self.column_adjust  # 修正列宽误差
+        for i in range(1, src_file.ws.max_row + 1):
+            tag_file.ws.row_dimensions[i + origin_row -
+                                       1].height = src_file.ws.row_dimensions[i].height
+        for i in range(1, src_file.ws.max_column + 1):
+            tag_file.ws.column_dimensions[get_column_letter(
+                i + origin_col - 1)].width = src_file.ws.column_dimensions[get_column_letter(i)].width + self.column_adjust  # 修正列宽误差
 
-        return self.tag_file
+        return tag_file
 
 
 # 调试
 if __name__ == '__main__':
     src_file_path = r'D:\codes\Python Projects\Excel-operate\tests\示例.xlsx'
     copyer = SheetCopy(src_file_path)
-    tag_file = copyer.copy_sheet(origin_col=5)
+    tag_file = copyer.copy_sheet(origin_col=5, origin_row=8)
     tag_file.save(r'D:\Desktop\1111.xlsx')
